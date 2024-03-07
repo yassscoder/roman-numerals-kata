c69f12f7156686b9c6b8eda48632d2531f7b1054
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-07 12:06:24 +0100

    GREEN: return VI when number is 6

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,8 +1,13 @@
 public class RomanNumerals {
     public static String convert(int arabicNumber) {
         String romanNumeral = "";
-        if (arabicNumber == 5) {
-            return "V";
+        if (arabicNumber >= 5) {
+            romanNumeral += "V";
+        }
+        if (arabicNumber - 5 > 0) {
+            for (int i = 0; i < arabicNumber - 5; i++) {
+                romanNumeral += "I";
+            }
         }
         if (arabicNumber < 5) {
             for (int i = 0; i < arabicNumber; i++) {
