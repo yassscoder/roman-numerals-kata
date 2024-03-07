fab3946b821c39afd70196a2e62b55c142b859be
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-07 11:02:43 +0100

    GREEN: return II when number is 2

---
+++ src/main/java/RomanNumerals.java
@@ -0,0 +1,8 @@
+public class RomanNumerals {
+    public static String convert(int arabicNumber) {
+        if (arabicNumber == 2) {
+            return "II";
+        }
+        return "I";
+    }
+}
