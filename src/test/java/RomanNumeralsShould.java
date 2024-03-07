9be309ee50cb05b11c34c235f68a90f02b745efe
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-07 10:54:00 +0100

    RED: return I if number is 1

--- src/test/java/RomanNumeralsShould.java
+++ src/test/java/RomanNumeralsShould.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanNumeralsShould {
+    @Test
+    @DisplayName("return I when arabic number is 1")
+    void return_I_if_number_1() {
+        assertEquals("I", RomanNumerals.convert(1));
+    }
 }
