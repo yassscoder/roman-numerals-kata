c0d2d088154171e3014f087a32baadfbb47a3846
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-07 13:40:12 +0100

    GREEN: return IV when number is 4

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,6 +1,9 @@
 public class RomanNumerals {
     public static String convert(int arabicNumber) {
         String romanNumeral = "";
+        if (arabicNumber == 4) {
+            return "IV";
+        }
         if (arabicNumber < 5) {
             for (int i = 0; i < arabicNumber; i++) {
                 romanNumeral += "I";
