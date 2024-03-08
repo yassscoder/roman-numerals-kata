70ce342fb273a58e5f0c855b90baf9d0cf571485
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-08 16:41:18 +0100

    refactor: change map to sortedMap to improve iteration order

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,15 +1,17 @@
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class RomanNumerals {
 
     public static String convert(int arabicNumber) {
-        Map<Integer, String> numbersDictionary = Map.ofEntries(
+        SortedMap<Integer, String> numbersDictionary = new TreeMap<>(Map.ofEntries(
                 Map.entry(1, "I"),
                 Map.entry(4, "IV"),
                 Map.entry(5, "V"),
                 Map.entry(9, "IX"),
                 Map.entry(10, "X")
-        );
+        ));
         if (numbersDictionary.containsKey(arabicNumber)) {
             return numbersDictionary.get(arabicNumber);
         }
