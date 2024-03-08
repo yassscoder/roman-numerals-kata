69c2fbffa75f6a1ab65728162bd8a1c8f285ac57
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-08 14:54:23 +0100

    refactor: clean up tests

--- src/test/java/RomanNumeralsShould.java
+++ src/test/java/RomanNumeralsShould.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,31 +16,19 @@ public class RomanNumeralsShould {
         assertEquals(romanNumeral, RomanNumerals.convert(arabicNumber));
     }
 
-    @Test
-    @DisplayName("return V when number is 5")
-    void return_V_if_number_5() {
-        assertEquals("V", RomanNumerals.convert(5));
-    }
-
     @ParameterizedTest
-    @DisplayName("return roman numeral when number is bigger than 5")
+    @DisplayName("return roman numeral for 4-9")
     @CsvSource({
+            "4, IV",
+            "5, V",
             "6, VI",
             "7, VII",
             "8, VIII",
+            "9, IX"
     })
-    void return_roman_when_bigger_than_5(int arabicNumber, String romanNumeral) {
+    void return_roman_when_bigger_than_3(int arabicNumber, String romanNumeral) {
         assertEquals(romanNumeral, RomanNumerals.convert(arabicNumber));
     }
 
-    @Test
-    @DisplayName("return IV when number is 4")
-    void return_iv_when_number_is_4() {
-        assertEquals("IV", RomanNumerals.convert(4));
-    }
-    @Test
-    @DisplayName("return IX when number is 9")
-    void return_ix_when_number_is_9() {
-        assertEquals("IX", RomanNumerals.convert(9));
-    }
+
 }
