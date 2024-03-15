613052f29e9fd2d5cc7e14d384ea64a9633e7ed1
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-15 10:25:32 +0100

    green: 50 return L

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -10,7 +10,8 @@ public class RomanNumerals {
                 Map.entry(4, "IV"),
                 Map.entry(5, "V"),
                 Map.entry(9, "IX"),
-                Map.entry(10, "X")
+                Map.entry(10, "X"),
+                Map.entry(50, "L")
         )).descendingMap();
         if (numbersDictionary.containsKey(arabicNumber)) {
             return numbersDictionary.get(arabicNumber);
